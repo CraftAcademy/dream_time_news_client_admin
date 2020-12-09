d025d7957f858fb6be9a90522e4819551b9771f2
viktor <viktorkarlsson125@gmail.com>
2020-12-09 21:00:56 +0100

    modifying app.jsx to arrow function. and formating

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from "react"
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div>
+    <>
       Dream Time News
-    </div>
+    </>
   );
-}
+};
 
 export default App;
