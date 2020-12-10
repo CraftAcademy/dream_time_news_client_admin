a270ae9a227663e0ffc4af18642d0653fd50bc55
viktor <viktorkarlsson125@gmail.com>
2020-12-10 09:41:37 +0100

    deleting unnecesary files and code. adding security setting in cypress.json

--- cypress/integration/visitorCanVisitSite.feature.js
+++
@@ -1,6 +0,0 @@
-describe("Visitor can visit site", () => {
-  it("is expected to show name of site", () => {
-    cy.visit("/");
-    cy.get("[data-cy='siteName']").should("contain", "Dream Time News");
-  });
-});
