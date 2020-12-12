651e392663971a7da1106dc8e45bc98971767779
Rithreaksa <wonderfulreaksa@gmail.com>
2020-12-12 11:20:03 +0100

    Add login, submit and return back to login form functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,103 @@
 import React, { useState, useEffect } from "react";
 import LoginForm from "./components/LoginForm";
+import axios from "axios";
 
-function App() {
+const App = () => {
   const [authenticated, setAuthenticated] = useState(false);
-  const [open, setOpen] = useState(false);
+  const [loginForm, setloginForm] = useState(false);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    setAuthenticated(false);
+    const credentials = localStorage.getItem("credentials");
+    if (credentials) {
+      setAuthenticated(true);
+    }
   }, []);
 
-  const onSubmit = (event) => {
+  const auth = async (email, password) => {
+    let credentials = { email, password };
+    try {
+      let response = await axios.post(
+        "http://localhost:3000/api/auth/sign_in",
+        credentials
+      );
+      if (response.status === 200) {
+        return response;
+      }
+    } catch (error) {
+      return undefined;
+    }
+  };
+
+  const onSubmit = async (event) => {
     event.preventDefault();
-    let credentials = {
-      email: event.target.email.value.trim(),
-      password: event.target.password.value.trim(),
-    } 
-  };
-  
+    const email = event.target.email.value.trim();
+    const password = event.target.password.value.trim();
 
-  const renderForm = () => {
-    if (!open) {
-      return <></>;
+    const response = await auth(email, password);
+    if (response) {
+      const credentials = {
+        uid: response.headers.uid,
+        client: response.headers.client,
+        "access-token": response.headers["access-token"],
+        expiry: response.headers.expiry,
+      };
+      localStorage.setItem("credentials", JSON.stringify(credentials));
+      setAuthenticated(true);
+      setloginForm(false);
+    } else {
+      setMessage("Unsuccessful");
     }
+  };
+
+  const onShowLoginForm = () => {
+    setloginForm(true);
+  };
+
+  const onLogout = () => {
+    localStorage.removeItem("credentials");
+    setAuthenticated(false);
+  };
+
+  const render = () => {
     if (authenticated) {
-      return <></>;
+      return (
+        <>
+          <p>You have logged in successfully</p>
+          <button data-cy="logout-btn" onClick={onLogout}>
+            Logout
+          </button>
+        </>
+      );
     }
-    return <LoginForm onSubmit={onSubmit} />;
+
+    if (loginForm) {
+      return (
+        <>
+          <p>{message}</p>
+          <LoginForm onSubmit={onSubmit} />
+          <button
+            data-cy="back-btn"
+            onClick={() => {
+              setloginForm(false);
+            }}
+          >
+            Back
+          </button>
+        </>
+      );
+    }
+
+    return (
+      <div>
+        <button data-cy="login-btn" onClick={onShowLoginForm}>
+          Login
+        </button>
+      </div>
+    );
   };
 
-  return (
-    <>
-      <button
-        data-cy="login-btn"
-        onClick={() => {
-          setOpen(!open);
-        }}
-      >
-        Login
-      </button>
-      {renderForm()}
-    </>
-  );
-}
-
-// {open ? <LoginForm onSubmit={onSubmit} /> : <></>}
+  return render();
+};
 
 export default App;
