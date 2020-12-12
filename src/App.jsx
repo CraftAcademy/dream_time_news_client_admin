09926f94476da78c4afa9f939ea49b4c78ecd2f0
swardbladpaulo <paulo@swardblads.se>
2020-12-12 12:45:02 +0100

    create authentication.jsx, add react-redux, create rootReducer, modify app.jsx, create statefolder, reducersfolder, storefolder and configureStre file. Commit for pingpong

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,117 @@
-import React, { useState, useEffect } from "react";
-import LoginForm from "./components/LoginForm";
-import axios from "axios";
+import React from "react";
+import { useSelector } from 'react-redux';
+import Authentication from './components/Authentication';
 
 const App = () => {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [loginForm, setloginForm] = useState(false);
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    const credentials = localStorage.getItem("credentials");
-    if (credentials) {
-      setAuthenticated(true);
-    }
-  }, []);
-
-  const auth = async (email, password) => {
-    let credentials = { email, password };
-    try {
-      let response = await axios.post(
-        "http://localhost:3000/api/auth/sign_in",
-        credentials
-      );
-      if (response.status === 200) {
-        return response;
+  const auth = useSelector(state => state.auth)
+  return (
+    <>
+      <Authentication />
+      {auth &&
+        <p data-cy="flash-message">
+          {auth.message}
+        </p>
       }
-    } catch (error) {
-      return undefined;
-    }
-  };
-
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    const email = event.target.email.value.trim();
-    const password = event.target.password.value.trim();
-
-    const response = await auth(email, password);
-    if (response) {
-      const credentials = {
-        uid: response.headers.uid,
-        client: response.headers.client,
-        "access-token": response.headers["access-token"],
-        expiry: response.headers.expiry,
-      };
-      localStorage.setItem("credentials", JSON.stringify(credentials));
-      setAuthenticated(true);
-      setloginForm(false);
-    } else {
-      setMessage("Unsuccessful");
-    }
-  };
-
-  const onShowLoginForm = () => {
-    setloginForm(true);
-  };
-
-  const onLogout = () => {
-    localStorage.removeItem("credentials");
-    setAuthenticated(false);
-  };
-
-  const render = () => {
-    if (authenticated) {
-      return (
-        <>
-          <p>You have logged in successfully</p>
-          <button data-cy="logout-btn" onClick={onLogout}>
-            Logout
-          </button>
-        </>
-      );
-    }
-
-    if (loginForm) {
-      return (
-        <>
-          <p>{message}</p>
-          <LoginForm onSubmit={onSubmit} />
-          <button
-            data-cy="back-btn"
-            onClick={() => {
-              setloginForm(false);
-            }}
-          >
-            Back
-          </button>
-        </>
-      );
-    }
-
-    return (
-      <div>
-        <button data-cy="login-btn" onClick={onShowLoginForm}>
-          Login
-        </button>
-      </div>
-    );
-  };
-
-  return render();
-};
+    </>
+  )
+}
 
 export default App;
+  // const [authenticated, setAuthenticated] = useState(false);
+  // const [loginForm, setloginForm] = useState(false);
+  // const [message, setMessage] = useState("");
+
+  // useEffect(() => {
+  //   const credentials = localStorage.getItem("credentials");
+  //   if (credentials) {
+  //     setAuthenticated(true);
+  //   }
+  // }, []);
+
+  // const auth = async (email, password) => {
+  //   let credentials = { email, password };
+  //   try {
+  //     let response = await axios.post(
+  //       "http://localhost:3000/api/auth/sign_in",
+  //       credentials
+  //     );
+  //     if (response.status === 200) {
+  //       return response;
+  //     }
+  //   } catch (error) {
+  //     return undefined;
+  //   }
+  // };
+
+  // const onSubmit = async (event) => {
+  //   event.preventDefault();
+  //   const email = event.target.email.value.trim();
+  //   const password = event.target.password.value.trim();
+
+  //   const response = await auth(email, password);
+  //   if (response) {
+  //     const credentials = {
+  //       uid: response.headers.uid,
+  //       client: response.headers.client,
+  //       "access-token": response.headers["access-token"],
+  //       expiry: response.headers.expiry,
+  //     };
+  //     localStorage.setItem("credentials", JSON.stringify(credentials));
+  //     setAuthenticated(true);
+  //     setloginForm(false);
+  //   } else {
+  //     setMessage("Unsuccessful");
+  //   }
+  // };
+
+  // const onShowLoginForm = () => {
+  //   setloginForm(true);
+  // };
+
+  // const onLogout = () => {
+  //   localStorage.removeItem("credentials");
+  //   setAuthenticated(false);
+  // };
+
+  // const render = () => {
+  //   if (authenticated) {
+  //     return (
+  //       <>
+  //         <p>You have logged in successfully</p>
+  //         <button data-cy="logout-btn" onClick={onLogout}>
+  //           Logout
+  //         </button>
+  //       </>
+  //     );
+  //   }
+
+  //   if (loginForm) {
+  //     return (
+  //       <>
+  //         <p>{message}</p>
+  //         <LoginForm onSubmit={onSubmit} />
+  //         <button
+  //           data-cy="back-btn"
+  //           onClick={() => {
+  //             setloginForm(false);
+  //           }}
+  //         >
+  //           Back
+  //         </button>
+  //       </>
+  //     );
+  //   }
+
+  //   return (
+  //     <div>
+  //       <button data-cy="login-btn" onClick={onShowLoginForm}>
+  //         Login
+  //       </button>
+  //     </div>
+  //   );
+  // };
+
+  // return render();
+// };
+
+// export default App;
---
+++ src/state/reducers/rootReducer.js
@@ -0,0 +1,19 @@
+import initialState from '../store/initialState';
+
+const rootReducer = (state = initialState, action) => {
+
+  switch (action.type) {
+
+    case "SET_CURRENT_USER":
+      return {
+        ...state,
+        currentUser: action.payload,
+        auth: { message: 'You are logged in', status: true }
+      }
+    
+    default:
+      return state
+  }
+}
+
+export default rootReducer;
