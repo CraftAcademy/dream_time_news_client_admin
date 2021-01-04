0a6a81f664ed63cf6072b24ace14241a046832bc
emtalen <59577366+emtalen@users.noreply.github.com>
2021-01-04 12:57:57 +0100

    reverting some refactoring to make it a bit easier to follow

--- src/components/CreateArticleForm.jsx
+++ src/components/CreateArticleForm.jsx
@@ -1,18 +1,44 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Form, Input, TextArea, Button, Message } from "semantic-ui-react";
-import ArticlesService from "../modules/ArticlesService";
+import axios from 'axios';
+// import ArticlesService from "../modules/ArticlesService";
 
 const CreateArticleForm = () => {
   const dispatch = useDispatch();
-
   const message = useSelector((state) => state.createArticleMessage);
+
+  const createArticle = async (event) => {
+    event.preventDefault();
+    let headers = JSON.parse(localStorage.getItem("J-tockAuth-Storage"));
+    try {
+     let response = await axios.post(
+        "/articles",
+        {
+          article: {
+            title: event.target.title.value,
+            sub_title: event.target.input_sub_title.value,
+            content: event.target.input_content.value,
+          },
+        },
+        {
+          headers: headers,
+        }
+      );
+      dispatch({
+        type: "SET_ARTICLE_MESSAGE",
+        payload: response.data.message,
+      });
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <>
       <Form
         data-cy="create-article-form"
-        onSubmit={(event) => ArticlesService.create(event, dispatch)}
+        onSubmit={(event) => createArticle(event)}
       >
         <Form.Field
           data-cy="input-title"
