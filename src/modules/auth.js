f8b5b116032bb1444e1c55e62812c6ff565cac27
Rithreaksa <wonderfulreaksa@gmail.com>
2021-01-12 17:08:44 +0100

    fixing prefix in jtockauth

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,8 +1,7 @@
-import JtockAuth from "j-tockauth";
+import JtockAuth from 'j-tockauth';
 
 const auth = new JtockAuth({
   host: process.env.REACT_APP_API_URL,
-  prefixUrl: "/api",
 });
 
 const performAuthentication = async (e, dispatch) => {
@@ -13,21 +12,21 @@ const performAuthentication = async (e, dispatch) => {
       e.target.elements.email.value,
       e.target.elements.password.value
     );
-    if (response.data.role === "journalist") {
+    if (response.data.role === 'journalist') {
       dispatch({
-        type: "SET_CURRENT_USER",
+        type: 'SET_CURRENT_USER',
         payload: response.data,
       });
     } else {
       dispatch({
-        type: "SET_ERROR_MESSAGE",
-        payload: "You are not authorized to be here",
+        type: 'SET_ERROR_MESSAGE',
+        payload: 'You are not authorized to be here',
       });
-      localStorage.removeItem("J-tockAuth-Storage");
+      localStorage.removeItem('J-tockAuth-Storage');
     }
   } catch (error) {
     dispatch({
-      type: "SET_ERROR_MESSAGE",
+      type: 'SET_ERROR_MESSAGE',
       payload: error.response.data.errors[0],
     });
   }
